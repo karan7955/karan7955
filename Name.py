df603b82779c97d8323a286740cc1905e0da2d01
karan7955 <146817726+karan7955@users.noreply.github.com>
2024-09-20 03:44:12 +0530

    Update Name.py

--- Name.py
+++ Name.py
@@ -1,7 +1,1 @@
-height = int(input("enter your hight: "))
-extra = 0
-spaces = height - 1
-for i in range(1,height+1):
-	print(" "*spaces,"*"*i,"*"*extra)
-	spaces -= 1
-print((height-1)*" ","||")
+print{"Hello welcome")
